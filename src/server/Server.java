ded705d53593cf2b4fa9ef46ee1ff3f1bd149f70
Grace Tong <gracetongmin@gmail.com>
2024-04-02 10:12:31 +0800

    fixing empty offset

--- src/server/Server.java
+++ src/server/Server.java
@@ -235,10 +235,13 @@ public class Server {
             buffer.get(filenameBytes);
             String filename = new String(filenameBytes);
 
+            int ignore = buffer.getInt(); // just reading arbitrarily
             int lengthofbytesToRead = buffer.getInt();
             byte[] contentToAppend_bytes = new byte[lengthofbytesToRead];
             buffer.get(contentToAppend_bytes);
             String contentToAppend = new String(contentToAppend_bytes);
+
+            System.out.println(contentToAppend);
 
             // byte[] contentToAppend = new byte[buffer.remaining()];
             // buffer.get(contentToAppend);
